7590f5157620667e7a056244057ac3f740ab8fd5
Stasyay <tibeikina@inbox.ru>
2024-05-12 15:35:28 +0500

    add HW

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <ArticleComponent/>
+  </div>
+</template>
+
+<script>
+import ArticleComponent from './components/ArticleComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ArticleComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+a {
+    text-decoration: none;
+}
+
+summary {
+    display: block;
+}
+
+summary::-webkit-details-marker {
+    display: none;
+}
+
+body {
+    font-family: "Jost", sans-serif;
+    font-style: normal;
+    line-height: normal;
+}
+
+.center {
+    padding-left: calc(50% - 1200px / 2);
+    padding-right: calc(50% - 1200px / 2);
+}
+
+}
+</style>
---
+++ src/components/ArticleTagsComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="article__tag" :class="{article__tag_active}" @click="$emit('click')">{{tags.title}}</button>
+</template>
+
+<script>
+export default {
+  name: 'ArticleTagsComponent',
+  props: {
+    tag_active: { type: Boolean, default: false },
+    tags: { type: Object, default() {
+        return { title: ''}
+      } 
+    },
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
